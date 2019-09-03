260042f08d81ef1d70f24e1c8f388b12a208fa92
Murat Memis <h.murat.memis@gmail.com>
2019-09-03 14:24:07 +0200

    Adds Polymorphism to the project

--- src/kea/fitness/Main.java
+++ src/kea/fitness/Main.java
@@ -7,37 +7,36 @@ public class Main {
 
     public static void main(String[] args) {
 
-        List<Employee> employees = new ArrayList<>();
-        List<Member> members = new ArrayList<>();
+        List<Person> data = new ArrayList<>();
 
-        employees.add(new Employee("Claus", "221175-1011", Employee.EmployeeType.ADMIN));
-        employees.add(new Employee("Tove", "011080-1014", Employee.EmployeeType.INSTRUCTOR, 20));
-        employees.add(new Employee("Anna", "011080-1012", Employee.EmployeeType.ADMIN));
-        employees.add(new Employee("Henning", "011080-1014", Employee.EmployeeType.INSTRUCTOR, 15));
-        members.add(new Member("Morten", "130195-1303", Member.MemberType.FULL));
-        members.add(new Member("Martin", "221175-1011", Member.MemberType.BASIC));
-        members.add(new Member("Martina", "050970-1409", Member.MemberType.FULL));
-        members.add(new Member("Marcel", "221175-1011", Member.MemberType.BASIC));
-
-        List<Person> persons = new ArrayList<>();
-        persons.addAll(employees);
-        persons.addAll(members);
+        data.add(new Employee("Claus", "221175-1011", Employee.EmployeeType.ADMIN));
+        data.add(new Employee("Tove", "011080-1014", Employee.EmployeeType.INSTRUCTOR, 20));
+        data.add(new Employee("Anna", "011080-1012", Employee.EmployeeType.ADMIN));
+        data.add(new Employee("Henning", "011080-1014", Employee.EmployeeType.INSTRUCTOR, 15));
+        data.add(new Member("Morten", "130195-1303", Member.MemberType.FULL));
+        data.add(new Member("Martin", "221175-1011", Member.MemberType.BASIC));
+        data.add(new Member("Martina", "050970-1409", Member.MemberType.FULL));
+        data.add(new Member("Marcel", "221175-1011", Member.MemberType.BASIC));
 
         System.out.println("\nFITNESS EMPLOYEES");
         System.out.println("Name\tCpr\t\t\tHours\tSalary\tVacation");
-        for (Employee employee : employees) {
-            employee.printEmployeeInformation();
+        for (Person employee : data){
+            if (employee instanceof Employee) {
+                ((Employee)employee).printEmployeeInformation();
+            }
         }
 
         System.out.println("\nFITNESS MEMBERS");
         System.out.println("Name\tCpr\t\t\tMembership\tFee");
-        for (Member member : members) {
-            member.printMemberformation();
+        for (Person member : data) {
+            if (member instanceof Member) {
+                ((Member) member).printMemberInformation();
+            }
         }
 
         System.out.println("\nEMPLOYEES & MEMBERS");
         System.out.println("Name\tCpr");
-        for (Person person : persons) {
+        for (Person person : data) {
             person.printPersonInformation();
         }
 
